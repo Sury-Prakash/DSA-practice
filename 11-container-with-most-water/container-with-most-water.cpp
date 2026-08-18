43ab2520697eea957db229f24c0fa8929052b2cd
Sury-Prakash <suryprakash9795@gmail.com>
2026-08-18 22:41:05 +0530

    Time: 0 ms (100.00%) | Memory: 63.1 MB (11.15%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
       int maxArea(vector<int>& height){
-      int st=0, end=height.size()-1;
-      int maxWater=0;
 
-      while(st < end){
-        int wight=end-st;
-        int ht=min(height[st], height[end]);
-        int currWater = wight*ht;
-        maxWater=max(currWater,maxWater);
-         
-         height[st] < height[end] ? st++: end--;
-      }
-      return maxWater;
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int w=right-left;
+            int ht = min(height[left], height[right]);
+            int currentArea=w*ht;
+            maxArea = max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
     }
 };
+//       int st=0, end=height.size()-1;
+//       int maxWater=0;
+
+//       while(st < end){
+//         int wight=end-st;
+//         int ht=min(height[st], height[end]);
+//         int currWater = wight*ht;
+//         maxWater=max(currWater,maxWater);
+         
+//          height[st] < height[end] ? st++: end--;
+//       }
+//       return maxWater;
+//     }
+// };
 
 
 //       int maxWater=0;
